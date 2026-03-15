aba6d9193e7f877f3526332e1dbd3dceae45378d
pranavbond007 <147826030+pranavbond007@users.noreply.github.com>
2026-03-15 22:41:13 +0530

    Update apper.py

--- apper.py
+++ apper.py
@@ -7,7 +7,7 @@ import pandas as pd
 import pickle
 
 ## Load the trained model
-model=tf.keras.models.load_model('model.h5',compile=False)
+model=tf.keras.models.load_model('model.keras')
 
 
 with open("label_encoder_gender.pkl","rb") as file:
